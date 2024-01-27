03c4ca5f2844295498e9b4d52d953debb63fc9ae
daang0406 <70720467+daang0406@users.noreply.github.com>
2024-01-27 12:44:02 -0500

    Update Graficas generales.py
    se puso el icono

--- pages/Graficas generales.py
+++ pages/Graficas generales.py
@@ -7,6 +7,8 @@ import plotly.express as px
 
 st.title('Graficas generales')
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
+icono = Image.open("KIW_icono.ico")
+st.set_page_config(page_title="Graficas generales", page_icon=icono, layout="wide", )
 
 # drive
 scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
